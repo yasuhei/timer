4d75d292a0c878f486e3cda961f1b1c480893d15
Yasuhei13 <67479901+yasuhei@users.noreply.github.com>
2022-10-23 18:24:21 -0300

    home

---
+++ ignite-timer/src/pages/Home/index.tsx
@@ -0,0 +1,56 @@
+import { Play } from "phosphor-react";
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+} from "./styles";
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="">Vou trabalhar em</label>
+          <TaskInput
+            placeholder="Dê um nome para o seu projeto"
+            id="task"
+            list="task-info"
+          />
+
+          <datalist id="task-info">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Projeto 4" />
+          </datalist>
+
+          <label htmlFor="">Durante</label>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            max={60}
+          />
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton disabled type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  );
+}
--- ignite-timer/src/styles/global.ts
+++ ignite-timer/src/styles/global.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
 
@@ -10,13 +10,14 @@ export const GlobalStyle = createGlobalStyle`
 
 :focus {
     outline: 0;
-    box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']};
+    box-shadow: 0 0 0 2px ${(props) => props.theme["green-500"]};
 
 }
 
 body {
-    background: ${(props) => props.theme['gray-900']};
-    color: ${(props) => props.theme['gray-300']};
+    background: ${(props) => props.theme["gray-900"]};
+    color: ${(props) => props.theme["gray-300"]};
+    -webkit-font-smoothing: antialiased;
 }
 
 body, input, text-area,button {
@@ -25,4 +26,4 @@ body, input, text-area,button {
     font-size: 1rem;
 }
 
-`
+`;
