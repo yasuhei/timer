0d297f5f4e44e8f10abf90e86132424797d2f888
Yasuhei Nakamura <67479901+yasuhei@users.noreply.github.com>
2022-10-27 16:11:55 -0300

    timer finalizado

--- ignite-timer/src/context/CyclesConctext.tsx
+++ ignite-timer/src/context/CyclesConctext.tsx
@@ -1,5 +1,18 @@
-import { createContext, ReactNode, useReducer, useState } from "react";
-import { Cycle, cyclesReducer, ActionTypes } from "../reducers/cycles";
+import { differenceInSeconds } from "date-fns";
+import {
+  createContext,
+  ReactNode,
+  useReducer,
+  useState,
+  useEffect,
+} from "react";
+import {
+  ActionTypes,
+  addNewCycleAction,
+  interruptCycleAction,
+  markCurrentCycleAsFinish,
+} from "../reducers/cycles/actions";
+import { Cycle, cyclesReducer } from "../reducers/cycles/reducer";
 interface CreateCycleData {
   task: string;
   minutesAmount: number;
@@ -23,27 +36,44 @@ interface CyclesContextProps {
 export const CyclesContext = createContext({} as CycleContextType);
 
 export function CycleContextProvider({ children }: CyclesContextProps) {
-  const [cyclesState, dispatch] = useReducer(cyclesReducer, {
-    cycles: [],
-    activeCycleId: null,
-  });
-
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
-
+  const [cyclesState, dispatch] = useReducer(
+    cyclesReducer,
+    {
+      cycles: [],
+      activeCycleId: null,
+    },
+    () => {
+      const storedStateAsJSON = localStorage.getItem(
+        "@ignite-timer:cycles-state",
+      );
+      if (storedStateAsJSON) {
+        return JSON.parse(storedStateAsJSON);
+      }
+    },
+  );
   const { cycles, activeCycleId } = cyclesState;
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
+    if (activeCycle) {
+      return differenceInSeconds(new Date(), new Date(activeCycle.startDate));
+    }
+
+    return 0;
+  });
+
+  useEffect(() => {
+    const stateJSON = JSON.stringify(cyclesState);
+
+    localStorage.setItem("@ignite-timer:cycles-state", stateJSON);
+  }, [cyclesState]);
 
   function setSecondsPassed(seconds: number) {
     setAmountSecondsPassed(seconds);
   }
 
   function markCurrentCycleAsFinished() {
-    dispatch({
-      type: ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED,
-      payload: {
-        activeCycleId,
-      },
-    });
+    dispatch(markCurrentCycleAsFinish());
   }
   function createNewCycle(data: CreateCycleData) {
     const id = String(new Date().getTime());
@@ -54,23 +84,13 @@ export function CycleContextProvider({ children }: CyclesContextProps) {
       startDate: new Date(),
     };
 
-    dispatch({
-      type: ActionTypes.ADD_NEW_CYCLE,
-      payload: {
-        newCycle,
-      },
-    });
+    dispatch(addNewCycleAction(newCycle));
 
     setAmountSecondsPassed(0);
   }
 
   function interruptCurrentCycle() {
-    dispatch({
-      type: ActionTypes.INTERRUPT_CURRENT_CYCLE,
-      payload: {
-        activeCycleId,
-      },
-    });
+    dispatch(interruptCycleAction());
   }
   return (
     <CyclesContext.Provider
--- ignite-timer/src/pages/Home/Countown/index.tsx
+++ ignite-timer/src/pages/Home/Countown/index.tsx
@@ -20,7 +20,7 @@ export function Countdown() {
       interval = setInterval(() => {
         const secondDifference = differenceInSeconds(
           new Date(),
-          activeCycle.startDate,
+          new Date(activeCycle.startDate),
         );
 
         if (secondDifference >= totalSecond) {
